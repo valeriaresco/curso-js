b63cad1a6345fafa8fd64a6eed7612900cc635ae
valeriaresco <valeriaresco99@gmail.com>
2023-01-26 20:44:43 -0300

    primeiras aulas do curso de js

---
+++ 3_particularidades_js/js/scripts.js
@@ -0,0 +1,27 @@
+console.log("Primeiro");
+
+console.log ("Ultimo");
+
+console.log ("Mais um console.log");
+
+
+//Declara uma variável
+var a = 1;
+
+a = 'teste';
+
+console.log(a);
+
+// Este é um cometário
+
+
+/*
+Este 
+é 
+um
+comentário
+de 
+múltiplas 
+linhas
+
+*/
