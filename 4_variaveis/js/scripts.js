b319577440832015d9e2af4ad61f0ef486decded
valeriaresco <valeriaresco99@gmail.com>
2023-01-26 21:13:35 -0300

    variaveis caracteres especiais e numeros

--- 4_variaveis/js/scripts.js
+++ 4_variaveis/js/scripts.js
@@ -1,7 +1,20 @@
 var teste = 1;
 
+console.log(teste);
+
 nome = "João";
 
 teste = "Valéria";
 
 console.log(teste);
+console.log(nome);
+
+var $nome = 'ASD';
+var _nome = 'ASD2';
+
+console.log($nome);
+console.log(_nome);
+
+var nome5 = 'ASD3'
+
+console.log(nome5);
