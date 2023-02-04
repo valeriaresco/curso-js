1cc00fc722e1e7189d71757a7c19630af3d24837
valeriaresco <valeriaresco99@gmail.com>
2023-02-03 21:08:04 -0300

    tipo string

---
+++ 6_string/js/scripts.js
@@ -0,0 +1,23 @@
+var nome = "Valéria";
+var sobrenome = "Resco"
+
+var nomeCompleto = nome + ' ' + sobrenome;
+
+console.log(nome);
+console.log(nomeCompleto);
+console.log(typeof nome);
+
+console.log(typeof "asd");
+
+console.log(typeof "5.837374849");
+
+var frase = 'Esta é uma frase complexa';
+
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome + " " + frase);
+
+document.write("Olá");
+
+document.write('Ele disse: "Olá"')
